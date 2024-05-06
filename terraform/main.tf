28af6b30fb89f4f055afada65d88f89b235fb497
unknown <dezmondmaina@gmail.com>
2024-05-06 11:24:02 +0300

    terraform edits

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,7 +8,7 @@ module "oke" {
   region              = var.region
   tenancy_id          = var.tenancy_id
   compartment_id      = var.compartment_id
-  ssh_public_key_path = var.ssh_public_key_path
+  ssh_public_key      = var.ssh_public_key_path
   ssh_private_key_path = var.ssh_private_key_path
   
   kubernetes_version = var.kubernetes_version
@@ -28,6 +28,8 @@ module "oke" {
     bastion  = { newbits = 13, netnum = 0, dns_label = "bastion" }
     operator = { newbits = 13, netnum = 1, dns_label = "operator" }
     cp       = { newbits = 13, netnum = 2, dns_label = "cp" }
+	int_lb   = { newbits = 11, netnum = 16, dns_label = "ilb" }
+	pub_lb   = { newbits = 11, netnum = 17, dns_label = "plb" }
     workers  = { newbits = 2, netnum = 1, dns_label = "workers" }
   }
 
@@ -38,6 +40,15 @@ module "oke" {
   create_bastion        = true
   bastion_allowed_cidrs = ["0.0.0.0/0"]
   bastion_upgrade       = false
+  bastion_image_os            = "Oracle Linux" 
+  bastion_image_os_version    = "8"            
+  bastion_image_type          = "platform"
+  bastion_shape = {
+  shape            = "VM.Standard.E4.Flex",
+  ocpus            = 1,
+  memory           = 4,
+  boot_volume_size = 50
+  }
   
   
   #operator host
@@ -46,6 +57,16 @@ module "oke" {
   create_iam_resources       = true
   create_iam_operator_policy = "always"
   operator_install_k9s       = true
+  operator_image_os              = "Oracle Linux" 
+  operator_image_os_version      = "8"            
+  operator_image_type            = "platform"
+  operator_install_k9s       = true
+  operator_shape = {
+  shape            = "VM.Standard.E4.Flex",
+  ocpus            = 1,
+  memory           = 4,
+  boot_volume_size = 50
+  }
   
   
   create_cluster       = true 
