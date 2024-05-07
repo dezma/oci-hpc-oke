6d104800c4f08174d2236b641dc656121538e1c3
unknown <dezmondmaina@gmail.com>
2024-05-07 16:22:35 +0300

    readme update

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -6,7 +6,7 @@ variable "ssh_public_key" { type = string }
 variable "ssh_private_key_path" { type = string }
 
 variable hpc_image { default = "" }
-variable hpc_shape { default = "" }
+variable hpc_shape { default = "BM.Optimized3.36" }
 variable kubernetes_version { default = "v1.29.1" }
 variable cluster_type { default = "enhanced" }
 variable cluster_name { default = "oke-rdma-cluster" }
